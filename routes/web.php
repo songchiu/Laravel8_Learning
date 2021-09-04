21b1950182e9ce30a84a84081c86a0845b8cd254
songchiu <songchiu.tw@gmail.com>
2021-09-04 16:06:34 +0800

    route test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/get_test/{id}', function ($id) {
+    return 'hi~ the id is '.$id;
+});
+
+Route::get('/optional_para/{num?}', function ($num=20) {
+    return 'hi~ the number is '.$num;
+});
+
+Route::get('/constraint_para/{num}', function ($num) {
+    return 'hi~ the number is '.$num;
+})->where([
+    'num' => '[0-9]+'
+]);
+//another place to set constraint is at RouteServiceProvider.php
+//which can make setting to the whole web
