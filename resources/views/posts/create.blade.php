10b3d68d07528a3e6bcea0aa28106d6503e9f960
songchiu <songchiu.tw@gmail.com>
2021-09-13 14:38:30 +0800

    display error message in different ways

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,8 +9,25 @@
     @csrf
     
     <div><input type="text" name="title"></div>
+
+    @error('title')
+        {{-- display specific column's error --}}
+        <div>{{ $message }}</div>
+    @enderror
+
     <div><textarea name="content"></textarea></div>
     <div><input type="submit" value="Create"></div>
+
+    @if($errors->any())
+        {{-- display all the error of the form --}}
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </form>
 
 @endsection
